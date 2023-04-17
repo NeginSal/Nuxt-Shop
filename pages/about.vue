2cb2eb40da6696d665280d98646bf230ec2f1653
NeginSal <neginsaljooghi@gmail.com>
2023-04-17 11:32:44 +0330

    Adding pages

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+        temporibus quia iusto, quibusdam odio laboriosam neque alias nostrum
+        blanditiis voluptatem!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+        temporibus quia iusto, quibusdam odio laboriosam neque alias nostrum
+        blanditiis voluptatem!
+      </p>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style  scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+  </style>
