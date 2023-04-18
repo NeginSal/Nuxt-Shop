c79f2d40797d3c8357a0470bce29ce5a25c862cb
NeginSal <neginsaljooghi@gmail.com>
2023-04-18 09:57:38 +0330

    Route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eius, porro
+      nobis eum voluptatem harum doloribus quod exercitationem? Nam, ducimus.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style  scoped>
+</style>
