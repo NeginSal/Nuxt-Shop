f433daa5099ca100e024ed3d6ebeef66d43820f4
NeginSal <neginsaljooghi@gmail.com>
2023-04-22 11:51:03 +0330

    add tailwind.css

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/products">Online Shop</NuxtLink>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4">
+        <NuxtLink to="/products" class="font-bold">Online Shop</NuxtLink>
       </nav>
     </header>
     <!-- output the page content -->
-    <div><slot /></div>
-    <footer>
-      <ul>
+    <div class="container mx-auto p-4"><slot /></div>
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
+      <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
         <li><NuxtLink to="/products">Products</NuxtLink></li>
